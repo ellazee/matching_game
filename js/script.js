1fd8baa5e640bd2982e0cd76dbf090d4b4a306e1
Ella Erickson <ellazerickson@gmail.com>
2016-02-03 19:06:08 -0800

    added 2 player functionality and changed to sea theme

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ console.log("I loaded");
 
 $(document).ready(function() {
 
-var images = ["images/hippo.png", "images/taco.png", "images/whitecat.png", "images/jam.png", "images/broccoli.png", "images/plum.png", "images/cat.png", "images/jellyfish.png", "images/sloth.png", "images/penguin.png", "images/hippo.png", "images/taco.png", "images/whitecat.png", "images/jam.png", "images/broccoli.png", "images/plum.png", "images/cat.png", "images/jellyfish.png", "images/sloth.png", "images/penguin.png"];
+var images = ["images/steve_zissou.png", "images/clownfish.jpg", "images/dolphin.jpg", "images/jellyfish.jpg", "images/octopus.jpg", "images/pufferfish.jpg", "images/sea_anemone.jpg", "images/seahorse.jpg", "images/starfish.jpg", "images/whaleshark.jpg", "images/steve_zissou.png", "images/clownfish.jpg", "images/dolphin.jpg", "images/jellyfish.jpg", "images/octopus.jpg", "images/pufferfish.jpg", "images/sea_anemone.jpg", "images/seahorse.jpg", "images/starfish.jpg", "images/whaleshark.jpg"];
 var divs = $("#gameboard div");
 
 //console.log(divs);
@@ -12,6 +12,9 @@ $("div .card").flip();
 var clickCount = 0;
 var clickedFirst;
 var clickedSecond;
+var turnCounter = 0;
+var scoreCounter1 = 0;
+var scoreCounter2 = 0;
 
 var shuffle = function(array) {
   var m = array.length, t, i;
@@ -36,53 +39,81 @@ var n = 1;
 
 for (var i = 0; i < newImages.length; i++) {
 	$("div .card:nth-child(" + n +") .back").css("background-image", "url(" + newImages[i] + ")");
-	// console.log(n +" and "+ i); 
-	// console.log(newImages[i]);
-	// console.log($("div .card:nth-child(" + n +") .back"));
 	n++;
 };
-//console.log(newImages);
 
-// 	$(".clas").flip({
-// 		axis: "x",
-// 		trigger: "click"
+
+var whichPlayer = function() {
+		var playerTurn = document.getElementById('turn');
+		if (turnCounter % 2 !== 0) {
+		//	setTimeout(function(){
+			playerTurn.innerHTML="Turn: Player 1"} 
+			else {playerTurn.innerHTML = "Turn: Player 2"}		
+		//}
+	}	
+// var whatScore = function() {
+// 		var playerTurn = document.getElementById('turn');
+// 		if (turnCounter % 2 !== 0) && {
 		
-// });
+// 			playerTurn.innerHTML="Turn: Player 1"} 
+// 			else {playerTurn.innerHTML = "Turn: Player 2"}		
+// 		//}
+// 	}
+var whatScore = function() {
+	var player1Score = document.getElementById('player1');
+	var player2Score = document.getElementById('player2');
+	player1Score.innerHTML = "Player 1 Score: " + scoreCounter1;
+	player2Score.innerHTML = "Player 2 Score: " + scoreCounter2;
+}	
+		
+
+
 	$("div .card .front").on("click", function(){
 		clickCount++;
 		if(clickCount % 2 !== 0) {
 		clickedFirst = $(this);	
 		clickedFirstImage = $(this).siblings().css("background-image");
-		console.log(clickedFirst + " = odd"); 		
+		//console.log(clickedFirst + " = odd"); 		
 		} else { 
 			if (clickCount % 2 ===0) {
 				clickedSecond = $(this);
 			 	clickedSecondImage = $(this).siblings().css("background-image");
-				console.log(clickedSecond); };
+				//console.log(clickedSecond); 
+			};
 			
 				if (clickedSecondImage === clickedFirstImage) {
 					console.log("it's a match");
+
 					clickedFirst.parent().flip(true);
 					clickedSecond.parent().flip(true);
 					clickedFirst.parent().off('.flip');
-					clickedSecond.parent().off('.flip');
-					//alert("It's a match");
-					//$('div .card').flip('toggle');
-					//leave flipped
-					console.log(clickCount);
+					clickedSecond.parent().off('.flip'); 
+					turnCounter++;
+					if (turnCounter % 2 !== 0) {
+						scoreCounter1++
+					} else {
+						scoreCounter2++
+					}
+					console.log(scoreCounter2);
+					console.log(scoreCounter1);
+					swal({
+						title: "Match!",
+						text: "Nice work, you got a match!",
+						type: "success",
+						imageUrl: "css/steve_zissou.png",
+						confirmButtonText: "word"
+					});
+										
 				} else {
 					setTimeout(function(){
 						clickedFirst.parent().flip('toggle');
 						clickedSecond.parent().flip('toggle');
+						turnCounter++;
 					}, 1200);	
-					console.log("not a match")
-					//console.log(clickCount);
-					// flip clickedFirst and clickedSecond back 
-					//$("div .card").flip('toggle');
-					//(clickedSecond).flip(true);
-										
+					//console.log("not a match")										
 				}
-				
+				whichPlayer();
+				whatScore();
 			};
 		});			
 	//});
